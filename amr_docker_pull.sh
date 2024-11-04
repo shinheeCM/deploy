4a8e741b09d6a597f0e3bd67463f18b0fc13d4d7
Dj <debanikroy92@gmail.com>
2024-11-04 11:01:18 +0900

    wonbot/shinheeagv:amr_node_js

--- amr_docker_pull.sh
+++ amr_docker_pull.sh
@@ -5,5 +5,5 @@ docker pull wonbot/shinheeagv:lidar_node
 docker pull wonbot/shinheeagv:amr_slam_nav
 docker pull wonbot/shinheeagv:led_amr
 docker pull wonbot/shinheeagv:amr_battery
-docker pull wonbot/shinheeagv:node_js
+docker pull wonbot/shinheeagv:amr_node_js
 docker pull wonbot/shinheeagv:mongodb
